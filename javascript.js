bf703c2578cb5b1010fa03436f341387231d82b8
jose <joseih2010@hotmail.com>
2024-02-28 17:29:42 -0300

    Adicionado botoes, e feito o design inicial

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+  return parseInt(x) + parseInt(y);
+}
+
+function subtract(x, y) {
+  return parseInt(x) - parseInt(y);
+}
+
+function multiply(x, y) {
+  return parseInt(x) * parseInt(y);
+}
+
+function divide(x, y) {
+  return parseInt(x) / parseInt(y);
+}
+
+console.log(add(2.5, 3));
